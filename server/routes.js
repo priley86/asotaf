b754f906db52387ac431eed63acd1db92550773a
ser_dunk_the_lunk <ser_dunko_el_lunko@gmail.com>
2015-07-12 14:30:19 -0400

    Tagged theories through H. Added D3 concept explorer at /explorer.

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/tinfoil', require('./api/tinfoil'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
@@ -19,6 +19,15 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
+
+      // Test route for explorer
+      if (req.originalUrl.indexOf('/explorer') !== -1 || req.headers.referer.indexOf('/explorer') !== -1) {
+          var path = req.originalUrl
+          if (path == '/explorer') {path = 'index.html'}
+          res.sendfile(app.get('appPath') + '/explorer/' + path)
+          return
+      }
+
       res.sendfile(app.get('appPath') + '/index.html');
     });
 };
